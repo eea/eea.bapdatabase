4c40e82368229cbe7843db77cfd50fe261ba941b
nituacor <nituacor@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-09-15 14:33:21 +0000

    dependecies added

--- setup.py
+++ setup.py
@@ -1,28 +1,13 @@
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Author(s):
-# Cristian Romanescu, Eau De Web
-
 from setuptools import setup, find_packages
 
 setup(name='BAPDatabase',
-    version='0.0.1dev',
+    version='0.0.1',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[]
+    install_requires=['MySQL-python',
+        'z3c.sqlalchemy']
 )
