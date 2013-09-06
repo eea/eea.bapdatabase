830a314e92694daed96fa000cc87ce97e48b707b
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-09-06 11:39:13 +0200

    force to appear plone portlets in BAP

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='BAPDatabase',
-    version='0.3.0',
+    version='0.4.0',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro',
